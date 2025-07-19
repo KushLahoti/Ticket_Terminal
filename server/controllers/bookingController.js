64fa2d94196781475f9ec18f6daf6c57f1384ca4
KushLahoti <kushlahoti123@gmail.com>
2025-07-20 00:25:10 +0530

    feat: implement Inngest function to check payment status and release seats if payment is not completed

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,3 +1,4 @@
+import { inngest } from "../inngest/index.js";
 import Booking from "../models/Booking.model.js";
 import Show from "../models/Show.model.js"
 import stripe from 'stripe'
@@ -76,6 +77,14 @@ export const createBooking = async (req, res) => {
 
         booking.paymentLink = session.url
         await booking.save()
+
+        // Run Inngest Scheduler Funtion to check payment status after 10 minutes
+        await inngest.send({
+            name: 'app/checkpayment',
+            data: {
+                bookingId: booking._id.toString()
+            }
+        })
 
         res.json({ success: true, url: session.url })
 
